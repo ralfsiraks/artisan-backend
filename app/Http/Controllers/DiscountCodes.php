601000ec544b8906aa81b13c2b281f3c6deb0535
ralfsiraks <hyperblazexd@gmail.com>
2024-02-09 16:31:01 +0200

    placeholder

--- app/Http/Controllers/DiscountCodes.php
+++ app/Http/Controllers/DiscountCodes.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DiscountCodes extends Controller
 {
     public function checkCode(Request $request) {
-        return response('Deez nuts');
+        $code = $request->header('code');
+        $response = DB::table('discount_codes')
+            ->where('code',  $code)
+            ->get();
+        return response()->json($response);
     }
 }
